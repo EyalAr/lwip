cbe0833cbe8340261a67f151286b29720b311eb9
Eyal Arubas <eyalarubas@gmail.com>
2014-09-19 18:12:35 +0800

    tweaks tests

--- tests/03.safety/00.locks.js
+++ tests/03.safety/00.locks.js
@@ -15,99 +15,99 @@ describe('simultaneous operations locks', function() {
     });
 
     describe('image.resize lock', function() {
-        it('should throw an error', function() {
-            image.resize(100, 100, function() {});
+        it('should lock image', function() {
+            image.resize.bind(image, 100, 100, function() {}).should.not.throwError();
             image.scale.bind(image, 0.5, 0.5, function() {}).should.throwError();
         });
     });
 
     describe('image.scale lock', function() {
-        it('should throw an error', function() {
-            image.scale(0.5, 0.5, function() {});
+        it('should lock image', function() {
+            image.scale.bind(image, 0.5, 0.5, function() {}).should.not.throwError();
             image.rotate.bind(image, 10, function() {}).should.throwError();
         });
     });
 
     describe('image.rotate lock', function() {
-        it('should throw an error', function() {
-            image.rotate(10, function() {});
+        it('should lock image', function() {
+            image.rotate.bind(image, 10, function() {}).should.not.throwError();
             image.crop.bind(image, 10, 10, function() {}).should.throwError();
         });
     });
 
     describe('image.crop lock', function() {
-        it('should throw an error', function() {
-            image.crop(10, 10, function() {});
+        it('should lock image', function() {
+            image.crop.bind(image, 10, 10, function() {}).should.not.throwError();
             image.blur.bind(image, 1, function() {}).should.throwError();
         });
     });
 
     describe('image.blur lock', function() {
-        it('should throw an error', function() {
-            image.blur(1, function() {});
+        it('should lock image', function() {
+            image.blur.bind(image, 1, function() {}).should.not.throwError();
             image.sharpen.bind(image, 1, function() {}).should.throwError();
         });
     });
 
     describe('image.sharpen lock', function() {
-        it('should throw an error', function() {
-            image.sharpen(1, function() {});
+        it('should lock image', function() {
+            image.sharpen.bind(image, 1, function() {}).should.not.throwError();
             image.mirror.bind(image, 'x', function() {}).should.throwError();
         });
     });
 
     describe('image.mirror lock', function() {
-        it('should throw an error', function() {
-            image.mirror('x', function() {});
+        it('should lock image', function() {
+            image.mirror.bind(image, 'x', function() {}).should.not.throwError();
             image.border.bind(image, 10, function() {}).should.throwError();
         });
     });
 
     describe('image.border lock', function() {
-        it('should throw an error', function() {
-            image.border(10, function() {});
+        it('should lock image', function() {
+            image.border.bind(image, 10, function() {}).should.not.throwError();
             image.pad.bind(image, 10, 10, 10, 10, function() {}).should.throwError();
         });
     });
 
     describe('image.pad lock', function() {
-        it('should throw an error', function() {
-            image.pad(10, 10, 10, 10, function() {});
+        it('should lock image', function() {
+            image.pad.bind(image, 10, 10, 10, 10, function() {}).should.not.throwError();
             image.saturate.bind(image, 1, function() {}).should.throwError();
         });
     });
 
     describe('image.saturate lock', function() {
-        it('should throw an error', function() {
-            image.saturate(1, function() {});
+        it('should lock image', function() {
+            image.saturate.bind(image, 1, function() {}).should.not.throwError();
             image.lighten.bind(image, 1, function() {}).should.throwError();
         });
     });
 
     describe('image.lighten lock', function() {
-        it('should throw an error', function() {
-            image.lighten(1, function() {});
+        it('should lock image', function() {
+            image.lighten.bind(image, 1, function() {}).should.not.throwError();
             image.darken.bind(image, 1, function() {}).should.throwError();
         });
     });
 
     describe('image.darken lock', function() {
-        it('should throw an error', function() {
-            image.darken(1, function() {});
+        it('should lock image', function() {
+            image.darken.bind(image, 1, function() {}).should.not.throwError();
             image.hue.bind(image, 100, function() {}).should.throwError();
         });
     });
 
     describe('image.hue lock', function() {
-        it('should throw an error', function() {
-            image.hue(10, function() {});
+        it('should lock image', function() {
+            image.hue.bind(image, 10, function() {}).should.not.throwError();
             image.hslAdjust.bind(image, 100, 1, 1, function() {}).should.throwError();
         });
     });
 
     describe('image.hslAdjust lock', function() {
-        it('should throw an error', function() {
-            image.hslAdjust(100, 1, 1, function() {});
+        it('should lock image', function() {
+            image.hslAdjust.bind(image, 100, 1, 1, function() {}).should.not.throwError();
             image.resize.bind(image, 100, 100, function() {}).should.throwError();
         });
     });
--- tests/03.safety/01.releases.js
+++ tests/03.safety/01.releases.js
@@ -15,98 +15,98 @@ describe('failed ops lock release', function() {
     });
 
     describe('image.resize release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.resize.bind(image, 'foo', 'bar', function() {}).should.throwError();
             image.scale.bind(image, 0.5, 0.5, function() {}).should.not.throwError();
         });
     });
 
     describe('image.scale release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.scale.bind(image, 'foo', 'bar', function() {}).should.throwError();
             image.rotate.bind(image, 10, function() {}).should.not.throwError();
         });
     });
 
     describe('image.rotate release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.rotate.bind(image, 'foo', function() {}).should.throwError();
             image.crop.bind(image, 10, 10, function() {}).should.not.throwError();
         });
     });
 
     describe('image.crop release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.crop.bind(image, 'foo', 'bar', function() {}).should.throwError();
             image.blur.bind(image, 1, function() {}).should.not.throwError();
         });
     });
 
     describe('image.blur release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.blur.bind(image, 'foo', function() {}).should.throwError();
             image.sharpen.bind(image, 1, function() {}).should.not.throwError();
         });
     });
 
     describe('image.sharpen release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.sharpen.bind(image, 'foo', function() {}).should.throwError();
             image.mirror.bind(image, 'x', function() {}).should.not.throwError();
         });
     });
 
     describe('image.mirror release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.mirror.bind(image, 'foo', function() {}).should.throwError();
             image.border.bind(image, 10, function() {}).should.not.throwError();
         });
     });
 
     describe('image.border release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.border.bind(image, 'foo', function() {}).should.throwError();
             image.pad.bind(image, 10, 10, 10, 10, function() {}).should.not.throwError();
         });
     });
 
     describe('image.pad release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.pad.bind(image, 'foo', 'bar', 'foo', 'bar', function() {}).should.throwError();
             image.saturate.bind(image, 1, function() {}).should.not.throwError();
         });
     });
 
     describe('image.saturate release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.saturate.bind(image, 'foo', function() {}).should.throwError();
             image.lighten.bind(image, 1, function() {}).should.not.throwError();
         });
     });
 
     describe('image.lighten release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.lighten.bind(image, 'foo', function() {}).should.throwError();
             image.darken.bind(image, 1, function() {}).should.not.throwError();
         });
     });
 
     describe('image.darken release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.darken.bind(image, 'foo', function() {}).should.throwError();
             image.hue.bind(image, 100, function() {}).should.not.throwError();
         });
     });
 
     describe('image.hue release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.hue.bind(image, 'foo', function() {}).should.throwError();
             image.hslAdjust.bind(image, 100, 1, 1, function() {}).should.not.throwError();
         });
     });
 
     describe('image.hslAdjust release', function() {
-        it('should not throw an error', function() {
+        it('should release image lock', function() {
             image.hslAdjust.bind(image, 'foo', 'foo', 'foo', function() {}).should.throwError();
             image.resize.bind(image, 100, 100, function() {}).should.not.throwError();
         });
